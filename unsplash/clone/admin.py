e162f1780876c3f1cacf36e42ca95836e333758e
David Mwangi <njorogedavid13@gmail.com>
2017-11-17 15:05:48 +0300

    add admin and tables

--- unsplash/clone/admin.py
+++ unsplash/clone/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import collections,Post,User
 
 # Register your models here.
+class PostAdmin(admin.ModelAdmin):
+    filter_horizontal=('collection',)
+
+admin.site.register(collections)
+admin.site.register(Post,PostAdmin)
+admin.site.register(User)
--- unsplash/clone/models.py
+++ unsplash/clone/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
 # Create your models here.
+class collections(models.Model):
+    name=models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
+
+class User(models.Model):
+    first_name=models.CharField(max_length=50)
+    last_name=models.CharField(max_length=50,blank =True)
+
+    def __str__(self):
+        return self.first_name
+class Post(models.Model):
+    image=models.ImageField()
+    pub_date=models.DateTimeField(auto_now_add=True)
+    user=models.ForeignKey(User)
+    collection=models.ManyToManyField(collections)
