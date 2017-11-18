28433c42c5f9b9fab46939232e480f9d4d99c1ca
David Mwangi <njorogedavid13@gmail.com>
2017-11-18 15:00:15 +0300

    add code to query posts for more than one tag

--- unsplash/clone/views.py
+++ unsplash/clone/views.py
@@ -1,8 +1,31 @@
 from django.shortcuts import render
 from .models import Post,collections,User
+from django.http import HttpResponse,Http404
+from itertools import chain
+# result = list(chain(*docs))
+
 # Create your views here.
 def index(request):
     posts=Post.get_posts()
     tags=collections.get_collections()
     print(len(tags))
     return render(request,'index.html',{"posts":posts,"tags":tags})
+def post(request,post_id):
+    # try:
+
+    post=Post.objects.get(id=post_id)
+    posts_list=[]
+    tags=post.collection.all()
+    for tag in tags:
+        # other_posts=collections.objects.filter(collection=1)
+        qwerty=tag.post_set.all()
+        posts_list.append(qwerty)
+    results=list(chain(*posts_list))
+    print(len(results))
+        # posts_list=posts_list+qwerty
+        # print(len(other_posts))
+    # other_posts=Post.objects.get(collection=post.collection).distinct()
+    # print(len(posts_list))
+    # except DoesNotExist:
+    #     raise Http404()
+    return render(request,'post.html',{"post":post,"posts":results})
