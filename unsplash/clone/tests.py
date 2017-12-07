ab17555446b7b0b460c91f4f59d8f9c162897c29
David Mwangi <njorogedavid13@gmail.com>
2017-12-07 16:25:52 +0300

    add link url for images

--- unsplash/clone/tests.py
+++ unsplash/clone/tests.py
@@ -1,8 +1,11 @@
 from django.test import TestCase
 from .models import collections,Post,User
+from django.contrib.auth.models import User
 
 # Create your tests here.
 class PostTestClass(TestCase):
     #setup method
     def setup(self):
-        pass
+        self.david = User(username='davy',password='sdfsdf')
+        self.david.save()
+        self.new_post=Post(user=self.david)
